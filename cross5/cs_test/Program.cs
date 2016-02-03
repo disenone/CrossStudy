19be2d8ab5b4b2d8ed95db2194ca15061c73b890
legaldev <legaldev0@gmail.com>
2016-02-03 18:23:13 +0800

    add a test c# project

---
+++ cross5/cs_test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace cs_test
+{
+    struct MutableStruct
+    {
+        public int Value { get; set; }
+
+        public void SetValue(int newValue)
+        {
+            Value = newValue;
+        }
+    }
+
+    class MutableStructHolder
+    {
+        public MutableStruct Field;
+        public MutableStruct Property { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MutableStructHolder holder = new MutableStructHolder();
+            // Affects the value of holder.Field
+            holder.Field.SetValue(10);
+            // Retrieves holder.Property as a copy and changes the copy
+
+            holder.Property.SetValue(10);
+            holder.Property = holder.Field;
+            holder.Property.SetValue(11);
+
+            Console.WriteLine(holder.Field.Value);
+            Console.WriteLine(holder.Property.Value);
+            Console.WriteLine(ReferenceEquals(holder.Property, holder.Field));
+            Console.WriteLine(holder.Property.GetType());
+            //DateTime.Now();
+            //DateTime.no
+        }
+    }
+}
